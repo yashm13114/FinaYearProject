4cc293235df825525c53ddc21276aa65d09c503e
yashm13114 <yashm13114@gmail.com>
2023-09-23 14:48:24 -0400

    sec commit

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -2,24 +2,24 @@ const jwt = require('jsonwebtoken')
 const User = require("../model/UserSchema")
 
 const authenticate = async(req, res, next) => {
-    // try {
-    //     const token = req.cookies.jwt
-    //     const verifytoken = jwt.verify(token, process.env.SECRET_KEY);
-    //     console.log(verifytoken)
+    try {
+        const token = req.cookies.jwt
+        const verifytoken = jwt.verify(token, process.env.SECRET_KEY);
+        console.log(verifytoken)
 
-    //     const rootuser = await User.findOne({ _id: verifytoken - _id, "tokens.token": token })
-    //     if (!rootuser) {
-    //         throw new Error("user not found")
-    //     }
-    //     req.token = token
-    //     req.rootuser = rootuser
-    //     req.userId = rootuser._id
-    //     next();
+        const rootuser = await User.findOne({ _id: verifytoken - _id, "tokens.token": token })
+        if (!rootuser) {
+            throw new Error("user not found")
+        }
+        req.token = token
+        req.rootuser = rootuser
+        req.userId = rootuser._id
+        next();
 
-    // } catch (err) {
-    //     res.status(404)
-    //     console.log(err)
-    // }
+    } catch (err) {
+        res.status(404)
+        console.log(err)
+    }
 
 }
 
